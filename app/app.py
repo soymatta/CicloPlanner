1fd57c09a6119e84bde0c37a7f7b73a55d5b2120
Yassed Matta <yassed883@gmail.com>
2023-10-10 06:39:02 -0500

    Instalando VirtualEnv para Windows

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-'''
+
 from db.db import app
 from Routes.users_routes import users_routes
 from Routes.routes_routes import rutas_routes
@@ -11,7 +11,7 @@ app.register_blueprint(users_routes, url_prefix="/users")
 app.register_blueprint(rutas_routes, url_prefix="/routes")
 app.register_blueprint(precautionTips_routes, url_prefix="/precaution_tips")
 app.register_blueprint(comments_routes, url_prefix="/comments")
-'''
+
 
 def create_app():
     app = Flask(__name__)
