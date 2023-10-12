7081de3953aa8644164718e396474fcc26f9c372
Yassed Matta <yassed883@gmail.com>
2023-10-12 14:55:31 -0500

    CRUD en user.js

--- app/db/db.py
+++ app/db/db.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.template_folder = '../templates'
+app.static_folder = '..//static'
 
 app.config['SQLALCHEMY_DATABASE_URI']= "mysql+pymysql://root@localhost/cicloplanner"
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
--- app/static/js/users.js
+++ app/static/js/users.js
@@ -1,38 +1,84 @@
 // ----- GET ----- //
 function getUsers() {
-  let json;
-
-  fetch("/users/get", {
+  let options = {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
+      "User-Agent": "insomnia/8.1.0",
     },
-  })
+    body: "false",
+  };
+
+  fetch("http://localhost:5000/users/get", options)
     .then((response) => response.json())
-    .then((data) => {
-      consol.log(data);
-    })
-    .catch((error) => {
-      console.error("Error (Get User):", error);
-    });
-
-  return json;
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 }
-
 // ----- POST ----- //
-function postUsers() {
-  let json;
-  return json;
+function postUser(username, email, password, image, address) {
+  let options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "User-Agent": "insomnia/8.1.0",
+    },
+    body:
+      '{"username":"' +
+      username +
+      '","email":"' +
+      email +
+      '","password":"' +
+      password +
+      '","image":"' +
+      image +
+      '","address":"' +
+      address +
+      '"}',
+  };
+
+  fetch("http://localhost:5000/users/post", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 }
 
 // ----- PUT ----- //
-function putUsers() {
-  let json;
-  return json;
+function putUser(id, username, email, password, image, address) {
+  let options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "User-Agent": "insomnia/8.1.0",
+    },
+    body:
+      '{"username":"' +
+      username +
+      '","email":"' +
+      email +
+      '","password":"' +
+      password +
+      '","image":"' +
+      image +
+      '","address":"' +
+      address +
+      '"}',
+  };
+
+  fetch("http://localhost:5000/users/put/" + id, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 }
 
 // ----- DELETE ----- //
-function deleteUsers() {
-  let json;
-  return json;
+function deleteUser(id) {
+  let options = {
+    method: "DELETE",
+    headers: { "User-Agent": "insomnia/8.1.0" },
+  };
+
+  fetch("http://localhost:5000/users/delete/" + id, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 }
